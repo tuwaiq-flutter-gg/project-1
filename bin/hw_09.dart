97302b1cda52edabc01405fd2b1b12bfcedf0702
Rakan Alotaibi <rakanalotaibi@Rakans-MacBook-Pro.local>
2022-11-17 14:49:46 +0300

    first commit

---
+++ bin/hw_09.dart
@@ -0,0 +1,3 @@
+import 'package:hw_09/hw_09.dart' as hw_09;
+
+void main(List<String> arguments) {}
---
+++ test/hw_09_test.dart
@@ -0,0 +1,8 @@
+import 'package:hw_09/hw_09.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
