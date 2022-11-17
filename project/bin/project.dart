8bdf03559be126b809c1ab8e2260a40b2308a839
FasialG <117646880+FasialG@users.noreply.github.com>
2022-11-17 15:22:18 +0300

    Creating classes

--- project/bin/project.dart
+++ project/bin/project.dart
@@ -1,1 +1,36 @@
 void main() {}
+
+abstract class Library {
+  add();
+  delete();
+  edit();
+  viewInformation();
+  search();
+  sell();
+}
+
+class Book extends Library {
+  int? book_id;
+  String? book_title;
+  String? author;
+  double? price;
+  int? quantity;
+
+  @override
+  add() {}
+
+  @override
+  delete() {}
+
+  @override
+  edit() {}
+
+  @override
+  search() {}
+
+  @override
+  sell() {}
+
+  @override
+  viewInformation() {}
+}
